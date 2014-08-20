b90d3301720778a5bdc41a65d8baff6de774c902
Anam <jagadijo@gmail.com>
2014-08-20 21:04:30 +0700

    update query loc

--- application/models/loc_m.php
+++ application/models/loc_m.php
@@ -12,10 +12,6 @@ class Loc_m extends MY_Model
    {
       $sql = "
          SELECT * FROM `ref_lang`
-           LEFT JOIN `trans_project_lang`
-             ON `lang_id` = `pl_lang_id`
-         WHERE pl_project_id = ? OR '' = ''
-         ORDER BY lang_id ASC
       ";
       $query = $this->db->query($sql, array($project_id));
       return $query->result();

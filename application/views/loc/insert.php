b3d21be5d4619d2cfa82168be14ab89d6ad93cc7
Anam <jagadijo@gmail.com>
2014-08-20 14:38:28 +0700

    add view insert LOC

---
+++ application/views/loc/insert.php
@@ -0,0 +1,32 @@
+<div class="row">
+	<div class="col-lg-12">
+		<h2 class="page-header">LOC Converter</h2>
+	</div>
+	<!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+
+<div class="row">
+	<div class="col-lg-12">
+		<h3>Project : <?php echo $project->project_name?></h3>
+		<div class="table-responsive">
+			<table class="table table-striped table-bordered table-hover">
+				<thead>
+					<tr>
+						<th style="width: 5%">#</th>
+						<th style="width: 8%">Check</th>
+						<th>Language</th>
+						<th style="width: 25%">Average SLOC per Function Point</th>
+					</tr>
+				</thead>
+				<tbody>
+				  <tr>
+						<td colspan="4" style="text-align: center; font-style: italic;">--
+							Data Not Found --</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<!-- /.row -->
